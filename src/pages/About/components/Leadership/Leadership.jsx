6504b5544650a6e68d95114e99324501ce90b504
Ebru Odok <ebruodok@berkeley.edu>
2020-08-21 01:34:26 -0400

    started work with cards

--- src/pages/About/components/Leadership/Leadership.jsx
+++ src/pages/About/components/Leadership/Leadership.jsx
@@ -1,19 +1,56 @@
 import React from 'react';
 import './Leadership.scss';
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+import CardGroup from 'react-bootstrap/CardGroup'
 
 const Leadership = () => {
   return (
     <section className="about-leadership">
-      <h2>Leadership</h2>
+    <div className="about-leadership-container">
+      <div className="about-leadership-beginning">
+        <h2>Leadership</h2>
 
-      <p>
-        Coming soon.
-      </p>
+          <p>
+          PCS@Berkeley holds yearly elections for
+          club members to fill the board committee. Our board is made up of
+          eight roles with the following responsibilities:
+          </p>
 
-      <p>
-      {/* TODO: Add More */}
-      </p>
+        </div>
+        <div className="about-roles-container">
+        <p>Coming soon...</p>
+        </div>
+        <CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
 
+</CardDeck>
+      </div>
     </section>
   );
 };
