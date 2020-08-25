10312d892ddb7d3a401d98ce2359874db7cc8a09
Ebru Odok <ebruodok@berkeley.edu>
2020-08-24 20:31:41 -0400

    cards done

--- src/pages/About/components/Leadership/Leadership.jsx
+++ src/pages/About/components/Leadership/Leadership.jsx
@@ -23,117 +23,108 @@ const Leadership = () => {
     <section className="about-leadership">
     <div className="about-leadership-container">
       <div className="about-leadership-beginning">
-        <h2>Leadership</h2>
+        <h2><center>Leadership</center></h2>
 
-          <p>
+          <p><center>
           PCS@Berkeley holds yearly elections for
           club members to fill the board committee. Our board is made up of
-          eight roles with the following responsibilities:
-          </p>
+          eight roles:
+          </center></p>
 
         </div>
       <div class="container">
     <div class="row">
         <div class="col-md-6 col-lg-4 col-xl-3 py-2">
             <div class="card bg-light border-dark h-100">
-              <div class="card-header">President</div>
                 <img class="card-img-top img-fluid" src={Anav} alt="Card image cap" />
                 <div class="card-block">
+                    <p class="card-text"><small class="text-muted"> </small></p>
                     <h4 class="card-title"><center>Anav Mittal</center></h4>
-                    <p class="card-text">Oversees club development and direction, manages board meetings, assists board members and project groups when additional help is needed.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text"><center>President</center></p>
+                    <p class="card-text"><small class="text-muted">(he/him)</small></p>
                 </div>
             </div>
         </div>
         <div class="col-md-6 col-lg-4 col-xl-3 py-2">
             <div class="card bg-light border-dark h-100">
-              <div class="card-header">Projects VP</div>
                 <img class="card-img-top img-fluid" src={Uzoma} alt="Card image cap" />
                 <div class="card-block">
+                    <p class="card-text"><small class="text-muted"> </small></p>
                     <h4 class="card-title"><center>Uzoma Nwakuche</center></h4>
-                    <p class="card-text">Develops project ideas and implementation throughout the semester.
-                    Oversees the progress throughout the semester, assisting in development when needed,
-                    and maintenance of the club’s codebase.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text"><center>Projects VP</center></p>
+                    <p class="card-text"><small class="text-muted">(he/him)</small></p>
                 </div>
             </div>
         </div>
         <div class="col-md-6 col-lg-4 col-xl-3 py-2">
             <div class="card bg-light border-dark h-100">
-              <div class="card-header">External VP</div>
                 <img class="card-img-top img-fluid" src={Ebru} alt="Card image cap" />
                 <div class="card-block">
+                    <p class="card-text"><small class="text-muted"> </small></p>
                     <h4 class="card-title"><center>Ebru Odok</center></h4>
-                    <p class="card-text">Leads recruitment efforts, manages social media platforms and webpage,
-                     collaborates with other student organizations to host workshops and events. </p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text"><center>External VP</center></p>
+                    <p class="card-text"><small class="text-muted">(she/her)</small></p>
                 </div>
             </div>
         </div>
-         <div class="col-md-6 col-lg-4 col-xl-3 py-2">
+        <div class="col-md-6 col-lg-4 col-xl-3 py-2">
             <div class="card bg-light border-dark h-100">
-              <div class="card-header">Internal VP</div>
                 <img class="card-img-top img-fluid" src={Elyse} alt="Card image cap" />
                 <div class="card-block">
+                    <p class="card-text"><small class="text-muted"> </small></p>
                     <h4 class="card-title"><center>Elyse Garreau</center></h4>
-                    <p class="card-text"><small class="text-muted">she/her</small></p>
-                    <p class="card-text">Oversees all internal affairs, leads weekly meetings,
-                    assists in planning and execution of all clubwide events.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text"><center>Internal VP</center></p>
+                    <p class="card-text"><small class="text-muted">(she/her)</small></p>
                 </div>
             </div>
         </div>
-         <div class="col-md-6 col-lg-4 col-xl-3 py-2">
+        <div class="col-md-6 col-lg-4 col-xl-3 py-2">
             <div class="card bg-light border-dark h-100">
-              <div class="card-header">DeCal Chair</div>
                 <img class="card-img-top img-fluid" src={Tyler} alt="Card image cap" />
                 <div class="card-block">
+                    <p class="card-text"><small class="text-muted"> </small></p>
                     <h4 class="card-title"><center>Tyler Nunez</center></h4>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text"><center>DeCal Chair</center></p>
+                    <p class="card-text"><small class="text-muted">(he/his)</small></p>
                 </div>
             </div>
         </div>
         <div class="col-md-6 col-lg-4 col-xl-3 py-2">
             <div class="card bg-light border-dark h-100">
-              <div class="card-header">Industry Chair</div>
                 <img class="card-img-top img-fluid" src={Chenyu} alt="Card image cap" />
                 <div class="card-block">
+                    <p class="card-text"><small class="text-muted"> </small></p>
                     <h4 class="card-title"><center>Chenyu Shi</center></h4>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text"><center>Industry Chair</center></p>
+                    <p class="card-text"><small class="text-muted">(he/his)</small></p>
                 </div>
             </div>
         </div>
         <div class="col-md-6 col-lg-4 col-xl-3 py-2">
-           <div class="card bg-light border-dark h-100">
-              <div class="card-header">Education Chair</div>
-               <img class="card-img-top img-fluid" src={Estelle} alt="Card image cap" />
-               <div class="card-block">
-                   <h4 class="card-title"><center>Estelle Rai</center></h4>
-                   <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                   <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-               </div>
-           </div>
-       </div>
-       <div class="col-md-6 col-lg-4 col-xl-3 py-2">
-          <div class="card bg-light border-dark h-100">
-            <div class="card-header">Social Chair</div>
-              <img class="card-img-top img-fluid" src={Ebru} alt="Card image cap" />
-              <div class="card-block">
-                  <h4 class="card-title"><center>Sarat Kotthakota</center></h4>
-                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-          </div>
-      </div>
+            <div class="card bg-light border-dark h-100">
+                <img class="card-img-top img-fluid" src={Estelle} alt="Card image cap" />
+                <div class="card-block">
+                    <p class="card-text"><small class="text-muted"> </small></p>
+                    <h4 class="card-title"><center>Estelle Rai</center></h4>
+                    <p class="card-text"><center>Development Chair</center></p>
+                    <p class="card-text"><small class="text-muted">(she/her)</small></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-4 col-xl-3 py-2">
+            <div class="card bg-light border-dark h-100">
+                <img class="card-img-top img-fluid" src={Ebru} alt="Card image cap" />
+                <div class="card-block">
+                    <p class="card-text"><small class="text-muted"> </small></p>
+                    <h4 class="card-title"><center>Sarat Kotthakota</center></h4>
+                    <p class="card-text"><center>Social Chair</center></p>
+                    <p class="card-text"><small class="text-muted">(he/his)</small></p>
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
-
-</div>
     </div>
-
-
-
     </section>
   );
 };
